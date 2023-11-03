7a5f23c7932036b435d3f3f7a4286f55f0a61fe6
adison <adison.soares7@gmail.com>
2023-11-03 13:02:49 -0300

    :sparkles: Feat: starts the project base

---
+++ api_rest_testes/src/main/java/br/com/adison/api_rest_testes/ApiRestTestesApplication.java
@@ -0,0 +1,13 @@
+package br.com.adison.api_rest_testes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestTestesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestTestesApplication.class, args);
+	}
+
+}
---
+++ api_rest_testes/src/test/java/br/com/adison/api_rest_testes/ApiRestTestesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adison.api_rest_testes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestTestesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
