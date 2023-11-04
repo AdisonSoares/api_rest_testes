dbeac1d46d5175a717e9193f9ffa064d0b8cd62f
adison <adison.soares7@gmail.com>
2023-11-04 20:02:54 -0300

    :sparkles: Feat: implements the findById endpoint and adds a user to test in postman

---
+++ api_rest_testes/src/main/java/br/com/adison/api_rest_testes/controller/resources/UserResource.java
@@ -0,0 +1,17 @@
+package br.com.adison.api_rest_testes.controller.resources;
+
+import br.com.adison.api_rest_testes.model.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(new User(1, "Adison", "adison@gmail.com", "123"));
+    }
+}
