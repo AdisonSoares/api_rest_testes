032a197597e63b7dd3f19c4e3215117cc9773de0
adison <adison.soares7@gmail.com>
2023-11-07 09:27:21 -0300

    :sparkles: Feat: applies the use of the exception class

--- api_rest_testes/src/main/java/br/com/adison/api_rest_testes/model/service/implement/UserServiceImplement.java
+++ api_rest_testes/src/main/java/br/com/adison/api_rest_testes/model/service/implement/UserServiceImplement.java
@@ -2,6 +2,7 @@ package br.com.adison.api_rest_testes.model.service.implement;
 
 import br.com.adison.api_rest_testes.model.domain.Users;
 import br.com.adison.api_rest_testes.model.service.UserService;
+import br.com.adison.api_rest_testes.model.service.exceptions.ObjectNotFoundException;
 import br.com.adison.api_rest_testes.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,6 @@ public class UserServiceImplement implements UserService {
     @Override
     public Users findById(Integer id) {
         Optional<Users> object = repository.findById(id);
-        return object.orElse(null);
+        return object.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!"));
     }
 }
