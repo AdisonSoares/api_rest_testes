2dcca446728db1d70d457d98d0f76c6009508338
adison <adison.soares7@gmail.com>
2023-11-16 12:14:18 -0300

    :white_check_mark: Test: add JUnit execution test

---
+++ usuario_api_rest/src/test/java/br/com/adison/api_rest_testes/TesteJUnit.java
@@ -0,0 +1,15 @@
+package br.com.adison.api_rest_testes;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Classe criada para testar o funcionamento do JUnit
+ * @Funcionalidade: JUnit
+ */
+public class TesteJUnit {
+    @Test
+    public void verificarJUnit(){
+        Assertions.assertTrue(true);
+    }
+}
