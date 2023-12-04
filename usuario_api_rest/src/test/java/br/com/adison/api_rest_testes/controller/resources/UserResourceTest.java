2fff52edc88b285624de44940311cc0c437f6be3
adison <adison.soares7@gmail.com>
2023-12-04 14:41:15 -0300

    :white_check_mark: Test: add findAll test with documentation tweaks

--- usuario_api_rest/src/test/java/br/com/adison/api_rest_testes/controller/resources/UserResourceTest.java
+++ usuario_api_rest/src/test/java/br/com/adison/api_rest_testes/controller/resources/UserResourceTest.java
@@ -12,14 +12,19 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 class UserResourceTest {
     public static final Integer  ID = 1;
-    public static final String NOME_TESTE = "nomeTeste";
+    public static final Integer  INDEX_ZERO = 0;
+    public static final String NOME = "nomeTeste";
     public static final String EMAIL = "emailTeste@gmail.com";
     public static final String PASSWORD = "123";
 
@@ -46,15 +51,15 @@ class UserResourceTest {
 
     /**
      * @Funcionalidade_original_testada: Esse método é para testar o "findById" que retorna um objeto
-     * "ResponseEntity" de acordo com um id especificado nos parâmetros ou lança uma exception caso nao
-     * tenha no banco. (findById)<p>
+     * "ResponseEntity" do tipo "<UserDTO>" de acordo com um id especificado nos parâmetros ou lança
+     * uma exception caso nao tenha no banco. (findById)<p>
      *
      * @Nomeação: Quando executar "FindById" retorna um objeto "ResponseEntity" com sucesso, mapeando um
      * objeto "users" para "userDTO". (whenFindByIdThenReturnSucess).<p>
      *
      * @Mockito:
-     * - PRIMEIRA: É mockado o chamado do método "findById" passando qualquer/any número de Id e retornando
-     * um objeto "users" com sucesso.<p>
+     * - PRIMEIRA: É mockado o chamado do método "findById" da classe "UserServiceImplement"
+     * passando qualquer/any número de Id e retornando um objeto "users" com sucesso.<p>
      * - SEGUNDA: É mockado o mapeamento de qualquer tipo de objeto para qualquer tipo de objeto para
      * retornar um objeto do tipo "userDTO" com sucesso.<p>
      *
@@ -97,13 +102,72 @@ class UserResourceTest {
         Assertions.assertEquals(UserDTO.class, response.getBody().getClass());
 
         Assertions.assertEquals(ID, response.getBody().getId());
-        Assertions.assertEquals(NOME_TESTE, response.getBody().getName());
+        Assertions.assertEquals(NOME, response.getBody().getName());
         Assertions.assertEquals(EMAIL, response.getBody().getEmail());
         Assertions.assertEquals(PASSWORD, response.getBody().getPassword());
     }
 
+    /**
+     * @Funcionalidade_original_testada: Esse método é para testar o "findAll" que retorna uma lista
+     * de objetos "ResponseEntity" do tipo "<List<UserDTO>>" sem parâmetros. (findAll)<p>
+     *
+     * @Nomeação: Quando executar "FindAll" retorna um objeto "ResponseEntity" com sucesso, mapeando a lista
+     * de objetos "users" para "userDTO" e retornando essa lista no corpo. (whenFindAllThenReturnAListOfUserDTO).<p>
+     *
+     * @Mockito:
+     * - PRIMEIRA: É mockado o chamado do método "findAll" da classe "UserServiceImplement" e retornando
+     * uma lista de "users" com sucesso.<p>
+     * - SEGUNDA: É mockado o mapeamento de qualquer tipo de objeto para qualquer tipo de objeto para
+     * retornar um objeto do tipo "userDTO" com sucesso.<p>
+     *
+     * @Response: Ao chamar o método "findAll" da classe "UserResource" é possível armazenar seu
+     * retorno "ResponseEntity" do tipo "<List<UserDTO>>" para testar as assertivas.<p>
+     *
+     * @Assertivas:
+     * - PRIMEIRA: verifica se o response está nulo.<p>
+     * - SEGUNDA: verifica se o corpo/body do response está nulo.<p>
+     * - TERCEIRA: verifica se a mensagem do response corresponde a "HttpStatus.OK".<p>
+     * - QUARTA: verifica se a classe do responde corresponde a "ResponseEntity.class".<p>
+     * - QUINTA: verifica se o corpo do response está retornando uma classe "ArrayList.class" .<p>
+     * - SEXTA: verifica se no primeiro indice do corpo do response está retornado uma classe do tipo "UserDTO.class".<p>
+     * - SETIMA: verifica se o id do primeiro indice do corpo do response está sendo retornado o ID estatico.<p>
+     * - OITAVA: verifica se o nome do primeiro indice do corpo do response está sendo retornado o NOME estatico.<p>
+     * - NONA: verifica se o email do primeiro indice do corpo do response está sendo retornado o EMAIL estatico.<p>
+     * - DECIMA: verifica se a senha do primeiro indice do corpo do response está sendo retornado o PASSWORD estatico.<p>
+     *
+     * @Assertivas_notnull: Verifica se o objeto o classe passada está nulo.<p>
+     *
+     * @Assertivas_equals: Na primeira parte o atributo que deveria retornar e na segunda
+     * o que está retornando.<p>
+     *
+     * @Recomendação: Para que o teste seja completo é preciso verificar todos os atributos, quanto mais
+     * atributos testados mais seguro o sistema e os códigos serão.
+     */
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAListOfUserDTO() {
+        Mockito
+                .when(service
+                        .findAll())
+                .thenReturn(List.of(users));
+        Mockito
+                .when(mapper
+                        .map(any(), any()))
+                .thenReturn(userDTO);
+
+        ResponseEntity<List<UserDTO>> response = resource.findAll();
+
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.getBody());
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(ResponseEntity.class, response.getClass());
+        Assertions.assertEquals(ArrayList.class, response.getBody().getClass());
+
+        Assertions.assertEquals(UserDTO.class, response.getBody().get(INDEX_ZERO).getClass());
+        Assertions.assertEquals(ID, response.getBody().get(INDEX_ZERO).getId());
+        Assertions.assertEquals(NOME, response.getBody().get(INDEX_ZERO).getName());
+        Assertions.assertEquals(EMAIL, response.getBody().get(INDEX_ZERO).getEmail());
+        Assertions.assertEquals(PASSWORD, response.getBody().get(INDEX_ZERO).getPassword());
     }
 
     @Test
@@ -123,7 +187,7 @@ class UserResourceTest {
      * ao serem usados acontece uma exception do tipo nullpointerexception.<p>
      */
     private void startUser(){
-        users = new Users(ID, NOME_TESTE, EMAIL, PASSWORD);
-        userDTO = new UserDTO(ID, NOME_TESTE, EMAIL, PASSWORD);
+        users = new Users(ID, NOME, EMAIL, PASSWORD);
+        userDTO = new UserDTO(ID, NOME, EMAIL, PASSWORD);
     }
 }
